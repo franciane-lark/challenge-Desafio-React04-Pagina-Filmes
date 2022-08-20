dec131e8b36e7df21582a0c1f55d3eeb81dca8b5
Franciane Lark <franciane.lark019@gmail.com>
2022-08-19 20:47:47 -0400

    update desafio 4

--- desafio4-react/src/components/Movies.js
+++ desafio4-react/src/components/Movies.js
@@ -1,10 +1,9 @@
 import React from 'react';
 
-export default class App extends React.Component {
+export default class Movies extends React.Component {
   render(){
     return(
       <>
-      <h1>Hello word</h1>
       </>
     )
   }
